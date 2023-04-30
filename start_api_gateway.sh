0b7bcfec62a77c15a11067c3c07598ca6b806451
GroKurfuerst <jh4312@columbia.edu>
2023-04-29 21:53:26 -0700

    modified api gateway and add default value

--- start_api_gateway.sh
+++ start_api_gateway.sh
@@ -2,6 +2,13 @@
 public_ipv4=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
 # public_ipv4="15.164.178.173"
 
+EVAL_API_PORT=6001
+TTS_API_PORT=8061
+ASR_API_PORT=8067
+API_GATEWAY_PORT=19528
+FRONT_END_LINK="https://devbox.articulateai.cn"
+# EDUBOT_WEBSITE_PORT=
+
 #Fetch configuration
 while getopts a:b:c:d:e: flag
 do
@@ -10,12 +17,14 @@ do
         b) TTS_API_PORT=${OPTARG};;
         c) ASR_API_PORT=${OPTARG};;
         d) API_GATEWAY_PORT=${OPTARG};;
-        e) EDUBOT_WEBSITE_PORT=${OPTARG}
+        # e) EDUBOT_WEBSITE_PORT=${OPTARG}
+        e) FRONT_END_LINK=${OPTARG}
     esac
 done
 
 #Set up endpoints
-FRONT_END_LINK="http://$public_ipv4:$EDUBOT_WEBSITE_PORT"
+# FRONT_END_LINK="http://$public_ipv4:$EDUBOT_WEBSITE_PORT"
+# FRONT_END_LINK="https://$public_ipv4:$EDUBOT_WEBSITE_PORT"
 PRON_FLUENCY_SCORING_API="http://$public_ipv4:$EVAL_API_PORT/pron_fluency_score"
 REPORT_GEN_API="http://$public_ipv4:$EVAL_API_PORT/score_report"
 TTS_API="http://$public_ipv4:$TTS_API_PORT/tts"
