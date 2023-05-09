4a07aeffbb61ba7854c476461726aa4733bebc1b
GroKurfuerst <jh4312@columbia.edu>
2023-05-09 11:57:30 -0700

    add asr to eval

--- start_eval_api.sh
+++ start_eval_api.sh
@@ -1,8 +1,9 @@
 #Assume all services run in a same instance for now
-# public_ipv4=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
+public_ipv4=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
 # public_ipv4="15.164.178.173"
 
 EVAL_API_PORT=6001
+ASR_API="http://$public_ipv4:8067/home"
 
 #Fetch configuration
 while getopts a: flag
@@ -14,6 +15,7 @@ done
 
 #Run the container
 sudo docker run -p $EVAL_API_PORT:6001 \
+                -e ASR_API=$ASR_API
                 --gpus all \
                 --name eval-api-container \
                 -v $HOME/data/kaldi:/app/kaldi \
@@ -21,10 +23,11 @@ sudo docker run -p $EVAL_API_PORT:6001 \
                 -v $HOME/data/intel:/opt/intel \
                 eval-api
 
-# sudo docker run -p 6001:6001 \
+# sudo docker run -it -p 6001:6001 \
 #                 --gpus all \
 #                 --name eval-api-container \
 #                 -v $HOME/data/nltk_data:/root/nltk_data \
 #                 -v $HOME/data/kaldi:/app/kaldi \
 #                 -v $HOME/data/intel:/opt/intel \
+#                 --entrypoint /bin/bash \
 #                 eval-api
